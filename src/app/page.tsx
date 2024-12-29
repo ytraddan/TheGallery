acda962f1e81c93977ffb7ca06a1c0d0b05d9815
ytraddan <1nightshelter@gmail.com>
2024-12-29 13:05:00 +0300

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+const mockUrls = [
+  "https://utfs.io/f/Sf47aEru3JHDCsic8VtVIXgxAWfaoTntewBEZs54b3cqlijp",
+  "https://utfs.io/f/Sf47aEru3JHDClpdYrtVIXgxAWfaoTntewBEZs54b3cqlijp",
+  "https://utfs.io/f/Sf47aEru3JHDBcrW0Xxx5rXK2YLT84H0yGopAzPOCj1vmUhE",
+  "https://utfs.io/f/Sf47aEru3JHDNKeLAOICJutme4yKMo0gnGp9cLrwiNFQfR1B",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>YOOO</h1>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-1/6">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
