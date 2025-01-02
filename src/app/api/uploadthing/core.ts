7d5df77239635a54c2c0fa630fd5fd40f0928761
ytraddan <1nightshelter@gmail.com>
2025-01-02 15:50:25 +0300

    increase max file size

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -15,7 +15,7 @@ export const ourFileRouter = {
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
-      maxFileSize: "4MB",
+      maxFileSize: "64MB",
       maxFileCount: 10,
     },
   })
