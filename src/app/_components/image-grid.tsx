7b6f08fbe669eb6e8106b927ca085dded7a956a9
ytraddan <1nightshelter@gmail.com>
2025-01-03 16:11:44 +0300

    move images component

---
+++ src/app/_components/image-grid.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { getMyImages } from "~/server/queries";
+import Image from "next/image";
+
+
+
+export default async function ImageGrid() {
+  const images = await getMyImages();
+
+  return (
+    <div className="grid grid-cols-1 gap-6 p-8 sm:grid-cols-2 md:grid-cols-3 md:px-14 lg:grid-cols-4">
+      {images.map((image) => (
+        <Link
+          href={`/img/${image.id}`}
+          key={image.id}
+          className="overflow-hidden rounded-xl border transition-colors hover:bg-accent"
+        >
+          <div>
+            <Image
+              src={image.url}
+              className="aspect-video h-full w-full object-cover"
+              width={400}
+              height={300}
+              alt={image.name}
+            />
+          </div>
+          <div className="p-4">
+            <h2 className="text-center font-medium first-letter:capitalize">
+              {image.name.split(".").slice(0, -1).join(".")}
+            </h2>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,7 @@
-import Image from "next/image";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { getMyImages } from "~/server/queries";
-import Link from "next/link";
+import ImageGrid from "./_components/image-grid";
 
 export const dynamic = "force-dynamic";
-
-async function Images() {
-  const images = await getMyImages();
-
-  return (
-    <div className="grid grid-cols-1 gap-6 p-8 sm:grid-cols-2 md:grid-cols-3 md:px-14 lg:grid-cols-4">
-      {images.map((image) => (
-        <Link
-          href={`/img/${image.id}`}
-          key={image.id}
-          className="overflow-hidden rounded-xl border transition-colors hover:bg-accent"
-        >
-          <div>
-            <Image
-              src={image.url}
-              className="aspect-video h-full w-full object-cover"
-              width={400}
-              height={300}
-              alt={image.name}
-            />
-          </div>
-          <div className="p-4">
-            <h2 className="text-center font-medium first-letter:capitalize">
-              {image.name.split(".").slice(0, -1).join(".")}
-            </h2>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
-}
 
 export default function HomePage() {
   return (
@@ -45,7 +12,7 @@ export default function HomePage() {
         </div>
       </SignedOut>
       <SignedIn>
-        <Images />
+        <ImageGrid />
       </SignedIn>
     </>
   );
