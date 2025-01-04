3baa822ab222ef4fb15c4df1e5baef37b7bc48be
ytraddan <1nightshelter@gmail.com>
2025-01-04 12:56:02 +0300

    add loading spinner for image page

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 import SidePanel from "./side-panel";
+import { LoadingSpinnerSvg } from "~/app/_components/simple-upload-button";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
@@ -16,7 +16,10 @@ export default async function FullPageImageView(props: { id: number }) {
 
   return (
     <div className="flex flex-col p-8 md:flex-row md:space-x-5 md:px-16">
-      <div className="flex flex-1 items-center justify-center overflow-hidden rounded-xl border bg-card/65 backdrop-blur-xl md:aspect-video dark:bg-card/50">
+      <div className="relative flex flex-1 items-center justify-center overflow-hidden rounded-xl border bg-card/65 backdrop-blur-xl md:aspect-video dark:bg-card/50">
+        <div className="absolute inset-0 -z-10 flex items-center justify-center">
+          <LoadingSpinnerSvg />
+        </div>
         <Image
           src={image.url}
           alt={image.name}
