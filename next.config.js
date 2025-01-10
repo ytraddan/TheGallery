05f13b3694420deab6a948dfd3817ff5b48d05bb
ytraddan <1nightshelter@gmail.com>
2025-01-10 17:40:25 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,11 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    remotePatterns: [{ hostname: "utfs.io" }, { hostname: "img.clerk.com" }],
+    remotePatterns: [
+      { hostname: "utfs.io" },
+      { hostname: "img.clerk.com" },
+      { hostname: "*.ufs.sh" },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
